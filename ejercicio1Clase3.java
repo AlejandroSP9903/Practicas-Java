3a274c4f95d3aedf70ff363931e4d7446fb401bc
LuisAlejandroUmDev <alejo199905luis@gmail.com>
2025-11-19 21:51:51 -0500

    Add files via upload

---
+++ ejercicio1Clase3.java
@@ -0,0 +1,31 @@
+package clase_3_PROGRAMADOR;
+import java.util.Scanner;
+public class ejercicio1Clase3 {
+	
+	/* Vamos a realizar un ejercisio donde nos pida Nombre, Nacionalidad y si tiene o no cedula.
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+	Scanner datos = new Scanner (System.in);
+	
+	System.out.println("Por favor digite su edad.");
+	int edad = datos.nextInt();
+	datos.nextLine();
+	System.out.println("Muchas gracias, ahora por favor digite si es colombiano o extranjero.");
+	String nacionalidad = datos.nextLine();
+	System.out.println("¿Tiene cedula?, escriba true ó false.");
+	boolean cedula = datos.nextBoolean();
+	
+	if (edad >= 18 && nacionalidad.equalsIgnoreCase ("colombiano") && cedula) {
+		System.out.println("Puedes ingresar al sistema de votacion.");
+	}else if (edad >= 18 && !cedula) {
+		System.out.println("No puedes votar por no tener cedula.");
+	
+	}else if (edad < 18) {
+			System.out.println("No puedes votar por ser menor de edad.");
+		}else {
+			System.out.println("Lo sentimos, por el momento no puedes votar.");
+	 }datos.close();	
+	}
+  }
