de0f96acdf0e9b95c3156f94bdbb509954648f85
LuisAlejandroUmDev <alejo199905luis@gmail.com>
2025-11-21 14:15:59 -0500

    Update Ejercicio4Clase4CiclosyLogica.java

--- Ciclos/Ejercicio4Clase4CiclosyLogica.java
+++ Ciclos/Ejercicio4Clase4CiclosyLogica.java
@@ -15,11 +15,15 @@ public class Ejercicio4Clase4CiclosyLogica {
 		// Aqui cometi el error de que no puse el "numero != 0" puse fue "numero < 10", lo cual era incorrecto
 		int n = 0;
 		while( numero != 0 ) {
-			numero = numero / 10;
-			System.out.println(n);
+			numero = numero / 10;
 			n++;
 			
 		}
+/* El error que cometi aqui fue que puse a imprimir "n" dentro del ciclo, debia ponerlo fueras para que imprima El Ultimo valor y no 
+todos hasta ese.
+*/
+System.out.println(n);
+
 		datos.close();
 	}
 
